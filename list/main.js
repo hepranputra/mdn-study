15fd5777119c153f23ebf0cf99cb69a884cd5fab
Hepran Putra <hepranputra@gmail.com>
2024-02-08 15:23:30 +0700

    feat: dynamic LIST

---
+++ list/main.js
@@ -0,0 +1,31 @@
+// Create HTML elements
+const list = document.createElement('ul');
+const info = document.createElement('p');
+
+// Return the 1st element within the document.
+const html = document.querySelector('html');
+
+
+// info is the declared variable above.
+info.textContent = 'Below is a dynamic list. Click anywhere ' +
+                    'on the page to add a new list item. ' +
+                    'Click an existing list item to change its ' +
+                    'text to something else.';
+
+document.body.appendChild(info);
+document.body.appendChild(list);
+
+html.onclick = () => {
+    const listItem = document.createElement('li');
+    const listContent = prompt('What content do you want the list ' + 
+                               'item to have?');
+    listItem.textContent = listContent;
+    list.appendChild(listItem);
+
+    listItem.onclick = (e) => {
+        e.stopPropagation();
+        const listContent = prompt('Enter new content for your ' +
+                                   'list item');
+        this.textContent = listContent;
+    };
+};
